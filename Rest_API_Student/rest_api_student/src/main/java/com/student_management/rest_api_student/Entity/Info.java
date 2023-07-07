08585e4bcde8cfe9108a35a09cdbc506d2018c01
sachin_ghuge <ghugesachin0806@gmail.com>
2023-07-07 14:01:29 +0530

    first

---
+++ Rest_API_Student/rest_api_student/src/main/java/com/student_management/rest_api_student/Entity/Info.java
@@ -0,0 +1,73 @@
+package com.student_management.rest_api_student.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Info {
+
+    @Id
+    private int id;
+    private String name;
+    private String surname;
+    private String contact;
+    private String city;
+
+    public Info() {
+    }
+
+    public Info(int id, String name, String surname, String contact, String city) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.contact = contact;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Info [id=" + id + ", name=" + name + ", surname=" + surname + ", contact=" + contact + ", city=" + city
+                + "]";
+    }
+
+}
